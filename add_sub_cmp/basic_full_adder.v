97a6885a7b80d8dbfa2e97eb4fce82ea6cb585b7
mr-jg-813 <gomesjorel75@gmail.com>
2026-03-20 17:00:49 +0530

    Create basic_full_adder.v

---
+++ add_sub_cmp/basic_full_adder.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module f_add(
+    input a,
+    input b,
+    input cin,
+    output reg s,
+    output reg cout
+    );
+    
+always@ (*) begin
+s = a ^ b ^ cin;
+cout = (a&b) | (a&cin) | (b&cin); 
+end
+endmodule
